5e7bac3be5662bb45176d952c4282c7c7d32e72e
zadigg <62830716+zadigg@users.noreply.github.com>
2022-06-02 17:17:53 +0300

    Rewrite the heroku string

--- src/ApolloConfig.js
+++ src/ApolloConfig.js
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 const link = new HttpLink({
-  uri: process.env.HEROKU_URI,
+  uri: "https://boiling-stream-97721.herokuapp.com/",
   // Additional options
 });
 
